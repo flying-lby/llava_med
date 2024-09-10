c83a29a642609927122d5dd5ff71f505993b1d32
flying-lby <2230232178@qq.com>
2024-09-10 17:10:09 +0800

    update

--- LLaVA-Med/llava/run/eval/eval_classify.sh
+++ LLaVA-Med/llava/run/eval/eval_classify.sh
@@ -1,7 +1,7 @@
-python -m llava.eval.model_vqa \
+python -m llava.run.eval.model_vqa \
     --model-path /srv/lby/llava_med/checkpoints/llava-mistral_finetuning/ \
-    --question-file LLaVA-Med/llava/run/data/eval/test_prompt/Chest-X-ray_llava_val.jsonl \
+    --question-file ./data/eval/test_prompt/Chest-X-ray_llava_val.jsonl \
     --image-folder "" \
-    --answers-file LLaVA-Med/llava/run/data/eval/test_prompt/Chest-X-ray_llava_val_ans.jsonl \
+    --answers-file ./data/eval/test_prompt/Chest-X-ray_llava_val_ans.jsonl \
     --temperature 0 \
     --conv-mode vicuna_v1
